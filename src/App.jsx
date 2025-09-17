80129c6b9848d6823bb8e4253c728e4db7517261
Wilf <wilfred.anorma@gmail.com>
2025-09-17 17:17:29 +0800

    lesson 1 - Scafolding

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+const App = () => {
+  return (
+    <>
+      <div className="categories-container">
+        <div className="category-container">
+          {/** <img /> */}
+          <div className="category-body-container">
+            <h2>Hats</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/** <img /> */}
+          <div className="category-body-container">
+            <h2>Jackets</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/** <img /> */}
+          <div className="category-body-container">
+            <h2>Sneakers</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/** <img /> */}
+          <div className="category-body-container">
+            <h2>Women</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+      <div className="categories-container">
+        <div className="category-container">
+          {/** <img /> */}
+          <div className="category-body-container">
+            <h2>Men</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
