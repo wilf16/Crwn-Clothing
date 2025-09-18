e75436cb6bb32e5f5922d70e487923cb8b7518c1
Wilf <wilfred.anorma@gmail.com>
2025-09-18 14:58:35 +0800

    Navigation with ReactRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router";
+import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>;
+};
+
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop" element={<Shop />} />
+        </Route>
       </Routes>
     </>
   );
