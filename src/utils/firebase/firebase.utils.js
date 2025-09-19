83e0d2c8fcfb0c3ec42a4046865a453fba4bc126
Wilf <wilfred.anorma@gmail.com>
2025-09-19 17:10:14 +0800

    lesson 7 - creating authenticated user in firebase database

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -9,6 +9,8 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import { use } from "react";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -40,3 +42,26 @@ onAuthStateChanged(auth, (user) => {
 });
 
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log("User snapshot:", userSnapshot);
+  console.log("User exist:", userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, { displayName, email, createdAt });
+    } catch (error) {
+      console.log("Error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
