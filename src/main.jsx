a0f90107d6de822d2a44a00935c00265dce2438a
Wilf <wilfred.anorma@gmail.com>
2025-09-23 17:24:34 +0800

    lesson 19 - using categoriesMap

--- src/main.jsx
+++ src/main.jsx
@@ -4,17 +4,17 @@ import "./index.scss";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
 import { UserProvider } from "./contexts/user.context.jsx";
-import { ProductsProvider } from "./contexts/products.context.jsx";
+import { CategoriesProvider } from "./contexts/categories.context.jsx";
 import { CartProvider } from "./contexts/cart.context.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </StrictMode>
