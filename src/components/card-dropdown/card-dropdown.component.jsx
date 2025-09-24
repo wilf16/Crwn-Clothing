ad6d32fde7f1e028302733eaeaf68f52391d09d7
Wilf <wilfred.anorma@gmail.com>
2025-09-24 15:14:40 +0800

    styled components Cart Dropdown

--- src/components/card-dropdown/card-dropdown.component.jsx
+++ src/components/card-dropdown/card-dropdown.component.jsx
@@ -3,7 +3,11 @@ import { useNavigate } from "react-router";
 import { CartContext } from "../../contexts/cart.context";
 import CartItem from "../cart-item/cart-item.component";
 import Button from "../button/button.component";
-import "./card-dropdown.styles.scss";
+import {
+  CarDropdownContainer,
+  CartItems,
+  EmptyMessage,
+} from "./card-dropdown.styles";
 
 const CardDropdown = () => {
   const { cartItems, setIsCartOpen } = useContext(CartContext);
@@ -14,16 +18,17 @@ const CardDropdown = () => {
     setIsCartOpen(false);
   };
 
-  const closeCart = () => setIsCartOpen(false);
   return (
-    <div className="cart-dropdown-container">
-      <div className="cart-items">
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
-      </div>
+    <CarDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
       <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
-    </div>
+    </CarDropdownContainer>
   );
 };
 
