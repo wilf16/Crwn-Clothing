09d76e54ffec7c368da421f6adfc17628c91129d
Wilf <wilfred.anorma@gmail.com>
2026-03-06 07:59:38 +0800

    Setting up categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,25 @@
 const App = () => {
+  const categories = [
+    { id: 1, title: "Hats" },
+    { id: 2, title: "Jackets" },
+    { id: 3, title: "Sneakers" },
+    { id: 4, title: "Womens" },
+    { id: 5, title: "Mens" },
+  ];
   return (
     <>
       <div className="categories-container">
-        <div className="category-container">
-          {/** <img /> */}
-          <div className="category-body-container">
-            <h2>Hats</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      </div>
-      <div className="categories-container">
-        <div className="category-container">
-          {/** <img /> */}
-          <div className="category-body-container">
-            <h2>Jackets</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      </div>
-      <div className="categories-container">
-        <div className="category-container">
-          {/** <img /> */}
-          <div className="category-body-container">
-            <h2>Sneakers</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      </div>
-      <div className="categories-container">
-        <div className="category-container">
-          {/** <img /> */}
-          <div className="category-body-container">
-            <h2>Women</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      </div>
-      <div className="categories-container">
-        <div className="category-container">
-          {/** <img /> */}
-          <div className="category-body-container">
-            <h2>Men</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+        {categories.map(({ title }) => {
+          return (
+            <div className="category-container">
+              <div className="background-image" />
+              <div className="category-body-container">
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </>
   );
