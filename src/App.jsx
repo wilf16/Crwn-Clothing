3ddfa4bba1c9d82f695b18ac9f89c90910ae88b0
Wilf <wilfred.anorma@gmail.com>
2026-03-08 15:35:43 +0800

    React Router Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
-import { Routes, Route, Outlet } from "react-router";
+import { Routes, Route } from "react-router";
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I an the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
