5a161f96f63037e6d0e28e60a59f647f88955ecc
Wilf <wilfred.anorma@gmail.com>
2026-03-09 11:01:22 +0800

    Authentication with Firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,17 @@
+import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+
 const SignIn = () => {
-  return <div>Sign In</div>;
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
+
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+    </div>
+  );
 };
 
 export default SignIn;
