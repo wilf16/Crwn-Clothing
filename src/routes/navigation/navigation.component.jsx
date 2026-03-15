8a0883ac4b78dd641dabaa110f1c757c84ce38ae
Wilf <wilfred.anorma@gmail.com>
2026-03-15 15:14:49 +0800

    User Context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,14 @@
+import { useContext } from "react";
 import { Outlet, Link } from "react-router";
+
 import CrwnLogo from "../../assets/crown.svg?react";
+import { UserContext } from "../../contexts/user.context";
 
 import "./navigation.styles.scss";
+
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <>
       <div className="navigation">
