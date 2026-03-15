6dc9442580de452c90f523fc6d32d8da5d118200
Wilf <wilfred.anorma@gmail.com>
2026-03-15 15:45:09 +0800

    Re-rendering From Context

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
+
+import { UserContext } from "../../contexts/user.context";
+
 import "./sign-up-form.styles.scss";
 
 const defaultFormFields = {
@@ -16,6 +20,7 @@ const defaultFormFields = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
+  const { setCurrentUser } = useContext(UserContext);
 
   console.log(formFields);
 
@@ -34,10 +39,10 @@ const SignUpForm = () => {
         email,
         password,
       );
-      const userDoc = await createUserDocumentFromAuth(user, {
+      await createUserDocumentFromAuth(user, {
         displayName,
       });
-      console.log(userDoc);
+      setCurrentUser(user);
       resetFormFields();
     } catch (error) {
       if (error.code === "auth/email-already-in-use") {
