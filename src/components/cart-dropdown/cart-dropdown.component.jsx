ab395f649e9c575cbd0a1f0ac925383a306fdaff
Wilf <wilfred.anorma@gmail.com>
2026-03-18 07:05:26 +0800

    Add to Cart Pt.2

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,20 @@
+import { useContext } from "react";
+
 import CartItem from "../cart-item/cart-item.component";
 import Button from "../button/button.component";
+
+import { CartContext } from "../../contexts/cart.context";
 
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        {[].map((item) => (
-          <CartItem cartItem={item} />
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
       <Button>GO TO CHECKOUT</Button>
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -2,8 +2,19 @@ import { createContext, useState } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   // find if cartItems contains productToAdd
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id,
+  );
   // If found, increment quantity
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem,
+    );
+  }
   // return new array with modified cart items/ new cart item
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
 export const CartContext = createContext({
