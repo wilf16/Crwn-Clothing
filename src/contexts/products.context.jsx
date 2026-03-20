1b7187669fcf41f32acf0641f14bef978406febd
Wilf <wilfred.anorma@gmail.com>
2026-03-20 20:47:39 +0800

    addCollectionAndDocuments Pt. 2

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,6 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
-import SHOP_DATA from "../shop-data.js";
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 export const ProductsContext = createContext({
   products: [],
@@ -9,6 +9,7 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+
   const value = { products, setProducts };
 
   return (
