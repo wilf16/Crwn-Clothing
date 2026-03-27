54a35776014fe527cbe809879b9ffbe184388b50
Wilf <wilfred.anorma@gmail.com>
2026-03-27 16:09:51 +0800

    React-Redux: Installation

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,7 +6,7 @@ import { CartIconContainer, ShoppingIcon, ItemCount } from "./cart-icon.styles";
 
 const CartIcon = () => {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
-  console.log(cartCount);
+
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
   return (
     <CartIconContainer onClick={toggleIsCartOpen}>
