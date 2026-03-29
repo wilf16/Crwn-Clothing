10036cf5a7b9361b60a2297a9fa9d2357c7fde1e
Wilf <wilfred.anorma@gmail.com>
2026-03-29 16:43:50 +0800

    Categories Selectors

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,14 +1,7 @@
-import { Outlet } from "react-router";
 import Directory from "../../components/directory/directory.component";
 
 const Home = () => {
   return <Directory />;
-  return (
-    <div>
-      <Directory categories={categories} />
-      <Outlet />
-    </div>
-  );
 };
 
 export default Home;
