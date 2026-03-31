656a90f2fb73d2dcaf115d7c539e7692c868a0ba
Wilf <wilfred.anorma@gmail.com>
2026-03-31 09:27:46 +0800

    Migrating Cart Context to Redux Pt. 1

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
 
 import { selectCartItems } from "../../store/cart/cart.selector";
-import { setCartItems } from "../../store/cart/cart.action";
+import { addItemToCart } from "../../store/cart/cart.action";
 
 import {
   ProductCardContainer,
@@ -12,32 +12,13 @@ import {
   Price,
 } from "./product-card.styles";
 
-const addCartItem = (cartItems, productToAdd) => {
-  // find if cartItems contains productToAdd
-  const existingCartItem = cartItems.find(
-    (cartItem) => cartItem.id === productToAdd.id,
-  );
-  // If found, increment quantity
-  if (existingCartItem) {
-    return cartItems.map((cartItem) =>
-      cartItem.id === productToAdd.id
-        ? { ...cartItem, quantity: cartItem.quantity + 1 }
-        : cartItem,
-    );
-  }
-  // return new array with modified cart items/ new cart item
-  return [...cartItems, { ...productToAdd, quantity: 1 }];
-};
-
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
   const cartItems = useSelector(selectCartItems);
   const { name, price, imageUrl } = product;
 
-  const addProductToCart = () => {
-    const newCartItems = addCartItem(cartItems, product);
-    dispatch(setCartItems(newCartItems));
-  };
+  const addProductToCartHandler = () =>
+    dispatch(addItemToCart(cartItems, product));
 
   return (
     <ProductCardContainer>
@@ -48,7 +29,7 @@ const ProductCard = ({ product }) => {
       </Footer>
       <Button
         buttonType={BUTTON_TYPE_CLASSES.inverted}
-        onClick={addProductToCart}
+        onClick={addProductToCartHandler}
       >
         Add to cart
       </Button>
--- src/store/cart/cart.action.js
+++ src/store/cart/cart.action.js
@@ -1,8 +1,59 @@
 import { createAction } from "../../utils/reducer/reducer.utils";
 import { CART_ACTION_TYPES } from "./cart.types";
 
+const addCartItem = (cartItems, productToAdd) => {
+  // find if cartItems contains productToAdd
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id,
+  );
+  // If found, increment quantity
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem,
+    );
+  }
+  // return new array with modified cart items/ new cart item
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id,
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem,
+  );
+};
+
+const clearCartItem = (cartItems, cartItemToClear) =>
+  cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
+
+const setCartItems = (cartItems) =>
+  createAction(CART_ACTION_TYPES.SET_CART_ITEMS, cartItems);
+
+export const addItemToCart = (cartItems, productToAdd) => {
+  const newCartItems = addCartItem(cartItems, productToAdd);
+  return setCartItems(newCartItems);
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const newCartItems = removeCartItem(cartItems, cartItemToRemove);
+  return setCartItems(newCartItems);
+};
+
+export const clearItemFromCart = (cartItems, cartItemToClear) => {
+  const newCartItems = clearCartItem(cartItems, cartItemToClear);
+  return setCartItems(newCartItems);
+};
+
 export const setIsCartOpen = (bool) =>
   createAction(CART_ACTION_TYPES.SET_IS_CART_OPEN, bool);
-
-export const setCartItems = (cartItems) =>
-  createAction(CART_ACTION_TYPES.SET_CART_ITEMS, cartItems);
