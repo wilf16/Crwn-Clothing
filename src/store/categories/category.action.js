2a6c2ffc6be24576ff07aac5d512ef159b5d493e
Wilf <wilfred.anorma@gmail.com>
2026-04-02 07:26:58 +0800

    Asynchronous Redux: Redux-Thunk

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -2,4 +2,7 @@ import { createAction } from "../../utils/reducer/reducer.utils";
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
 export const setCategories = (categoriesArray) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+  createAction(
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray,
+  );
