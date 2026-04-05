30d786fd53754446908754ca4cc18b7627e9f7f0
Wilf <wilfred.anorma@gmail.com>
2026-04-05 11:12:00 +0800

    Setting up Our PaymentForm

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -1,5 +1,5 @@
 import { loadStripe } from "@stripe/stripe-js";
 
 export const stripePromise = loadStripe(
-  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY,
+  "pk_test_51TIdhfHB9u0D9rvzrem4oclYG2yQXou233AyBvV73XDH8np3H6xRDExbBE1rfPXNDa74to6OaUcGTwMQuSJjTmkt00yhu5Wrnr",
 );
