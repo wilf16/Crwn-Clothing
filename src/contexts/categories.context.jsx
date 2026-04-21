182e51df6575a9a528d312e4e3dfe115da60975b
Wilf <wilfred.anorma@gmail.com>
2026-04-21 08:54:51 +0800

    Updating Components To Consume GraphQL Values

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -26,8 +26,17 @@ export const CategoriesProvider = ({ children }) => {
   const { loading, error, data } = useQuery(GET_COLLECTIONS);
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  console.log("Loading", loading);
-  console.log("Data", data);
+  useEffect(() => {
+    if (data) {
+      const { collections } = data;
+      const collectionMap = collections.reduce((acc, collection) => {
+        const { title, items } = collection;
+        acc[title.toLowerCase()] = items;
+        return acc;
+      }, {});
+      setCategoriesMap(collectionMap);
+    }
+  }, [data]);
 
   const value = { categoriesMap };
 
