2411667fbdfebb8502eb44be4b52f32c858ec3f1
Wilf <wilfred.anorma@gmail.com>
2026-04-29 17:23:14 +0800

    Render with Providers and Cart Icon Tests

---
+++ src/components/cart-icon/__tests__/cart-icon.component.test.jsx
@@ -0,0 +1,24 @@
+import { screen } from "@testing-library/react";
+
+import { renderWithProviders } from "../../../utils/test/test.utils";
+import CartIcon from "../cart-icon.component";
+import { Quantity } from "../../checkout-item/checkout-item.styles";
+
+describe("Cart Icon test", () => {
+  test("Uses preloaded state to render", () => {
+    const initialCartItems = [
+      { id: 1, name: "Item A", imageUrl: "test", price: 10, quantity: 1 },
+    ];
+
+    renderWithProviders(<CartIcon />, {
+      preloadedState: {
+        cart: {
+          cartItems: initialCartItems,
+        },
+      },
+    });
+
+    const cartIconElement = screen.getByText("1");
+    expect(cartIconElement).toBeInTheDocument();
+  });
+});
---
+++ src/utils/test/test.utils.jsx
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { rootReducer } from "../../store/root-reducer";
+
+export function renderWithProviders(ui, extendedRenderOptions = {}) {
+  const {
+    preloadedState = {},
+    // Automatically create a store instance if no store was passed in
+    store = createStore(rootReducer, preloadedState),
+    ...renderOptions
+  } = extendedRenderOptions;
+
+  const Wrapper = ({ children }) => (
+    <Provider store={store}>{children}</Provider>
+  );
+
+  // Return an object with the store, user, and all of RTL's query functions
+  return {
+    store,
+    ...render(ui, { wrapper: Wrapper, ...renderOptions }),
+  };
+}
